742457a1c06952dd4fff1f626a6b679d29e56d7b
Cristian Ninja <cristianferrarys@gmail.com>
2020-08-23 02:44:31 -0300

    Init finger app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FingerprintAIO, FingerprintOptions } from '@ionic-native/fingerprint-aio/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  fingerprintOptions: FingerprintOptions;
+  constructor(private faio: FingerprintAIO , private platform: Platform ) {
+    this.fingerprintOptions = {
+      title: 'fingerprint-demo',
+      subtitle: 'password',
+      disableBackup: true
+  };
+  }
 
-  constructor() {}
+
+  async mostrar() {
+    try {
+      await this.platform.ready();
+      const finger = await this.faio.isAvailable();
+      console.log(finger);
+      if (finger === 'OK') {
+        const result = await this.faio.show(this.fingerprintOptions);
+        console.log(result);
+      }
+    }catch(e) {
+      console.error(e);
+    }
+
+  }
 
 }
